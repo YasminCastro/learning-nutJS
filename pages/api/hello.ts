a4df5981727cc94ec5cdc460cbb00e3be21fe513
Yasmin <yasminsdcastro@gmail.com>
2022-06-25 13:32:22 -0300

    teste

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,43 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  mouse,
+  straightTo,
+  Point,
+  centerOf,
+  screen,
+  Region,
+  Button,
+  up,
+  keyboard,
+  left,
+  right,
+  Key,
+} from "@nut-tree/nut-js";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  (async () => {
+    mouse.config.mouseSpeed = 1000;
+    keyboard.config.autoDelayMs = 100;
+    await mouse.setPosition(new Point(912, 1050));
+    await mouse.click(Button.LEFT);
+    await mouse.move(up(200));
+    await mouse.click(Button.LEFT);
+    await keyboard.type("teste");
+    await mouse.move(left(150));
+    await mouse.click(Button.LEFT);
+    await mouse.move(up(200));
+    await mouse.click(Button.LEFT);
+    await mouse.click(Button.LEFT);
+    await keyboard.pressKey(Key.Enter);
+  })();
+
+  res.status(200).json({ name: "John Doe" });
 }
